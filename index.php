35dc83734c96532438642804d422f4e35c228b1b
Macbook Pro <MacbookPro@Macbooks-MacBook-Pro-2.local>
2019-07-11 18:17:33 +1000

    corrected foreach

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ function listWrapper($content){
 
 <h1>My List of Animals</h1>
 <ul>
-<? foreach($animas as $animal) : ?>
+<? foreach($animals as $animal) : ?>
     <li><?= $animal ?></li>
 <? endforeach ?>
 </ul>
