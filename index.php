2f33aca9decd446c01493228da7eab98d9ab2299
Macbook Pro <MacbookPro@Macbooks-MacBook-Pro-2.local>
2019-07-16 19:05:47 +1000

    started fresh

---
+++ index.php
@@ -0,0 +1,38 @@
+
+<head>
+    <title>Foo News | Useless Information</title>
+</head>
+<body>
+    <h1>Latest News</h1>
+    <ul>
+
+    <? $x = 0;while ($x < 5){ ?>
+        <li>
+            <article class="card">
+                <h2>Post Title</h2>
+                <img src="http://placehold.it/300x200">
+                <p> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                <a href=""></a>
+            </article>
+        </li>
+
+<? $x++; } ?>
+        <li>
+            <article class="card">
+                <h2>Post Title</h2>
+                <img src="http://placehold.it/300x200">
+                <p> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                <a href=""></a>
+            </article>
+        </li>
+        <li>
+            <article class="card">
+                <h2>Post Title</h2>
+                <img src="http://placehold.it/300x200">
+                <p> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                <a href=""></a>
+            </article>
+        </li>
+    </ul>
+    
+</body>
