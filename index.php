164ccb4289407db78eb36ba52aaaf9bb4517b322
Macbook Pro <MacbookPro@Macbooks-MacBook-Pro-2.local>
2019-07-24 20:57:35 +1000

    setup bootstrap theme

--- index.php
+++ index.php
@@ -1,21 +1,47 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<? the_post() ?>
+
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-        <title>Tab Title</title>
-        <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" />
-    </head>
+        <title><?php the_title() ?></title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href=<?php echo get_stylesheet_uri()?>>
+    </head> 
     <body>
-        <h1>latest News</h1>
-        <ul class="row">
-            <? while (have_posts()) : the_post(); ?>
-                <li class="column">
-                    <div class="card">
-                        <h2 class="card-title"><? the_title() ?></h2>
-                        <div class="card-image"><? the_post_thumbnail(); ?></div>
-                        <div class="card-content"><? the_excerpt() ?></div>
-                    </div>
-                </li>
-            <? endwhile ?>
-        </ul>
+   
+        <div class="container">
+            <div class="row">
+                <div class="col-sm">
+                    LOGO
+                </div>
+                <div class="col-sm">
+                    <?php wp_nav_menu() ?>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="foo-banner position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center" style="background-image: url(<?php the_post_thumbnail_url() ?>)">
+            <div class="col-md-5 p-lg-5 mx-auto my-5">
+                <h1 class="display-4 font-weight-normal"><?php the_title() ?></h1>
+                <p class="lead font-weight-normal"><?php the_excerpt() ?></p>
+                <a class="btn btn-outline-secondary" href="#content">Read More</a>
+            </div>
+            <div class="product-device shadow-sm d-none d-md-block"></div>
+            <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+        </div>
+
+        <div class="container" id="content">
+            <div class="row">
+                <div class="col-sm">
+                    <?php the_content() ?>
+                </div>
+            </div>
+        </div>
+
+
+
     </body>
+
 </html>
