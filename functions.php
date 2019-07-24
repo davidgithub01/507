2fb37ac05e70accc663903f38708e46aedee4178
Macbook Pro <MacbookPro@Macbooks-MacBook-Pro-2.local>
2019-07-24 17:46:57 +1000

    added more stuff

---
+++ functions.php
@@ -0,0 +1,3 @@
+<?php 
+// show featured image in the post admin screen
+add_theme_support( 'post-thumbnails' );
--- index.php
+++ index.php
@@ -1,38 +1,21 @@
-
-<head>
-    <title>Foo News | Useless Information</title>
-</head>
-<body>
-    <h1>Latest News</h1>
-    <ul>
-
-    <? $x = 0;while ($x < 5){ ?>
-        <li>
-            <article class="card">
-                <h2>Post Title</h2>
-                <img src="http://placehold.it/300x200">
-                <p> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-                <a href=""></a>
-            </article>
-        </li>
-
-<? $x++; } ?>
-        <li>
-            <article class="card">
-                <h2>Post Title</h2>
-                <img src="http://placehold.it/300x200">
-                <p> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-                <a href=""></a>
-            </article>
-        </li>
-        <li>
-            <article class="card">
-                <h2>Post Title</h2>
-                <img src="http://placehold.it/300x200">
-                <p> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-                <a href=""></a>
-            </article>
-        </li>
-    </ul>
-    
-</body>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <title>Tab Title</title>
+        <link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" />
+    </head>
+    <body>
+        <h1>latest News</h1>
+        <ul class="row">
+            <? while (have_posts()) : the_post(); ?>
+                <li class="column">
+                    <div class="card">
+                        <h2 class="card-title"><? the_title() ?></h2>
+                        <div class="card-image"><? the_post_thumbnail(); ?></div>
+                        <div class="card-content"><? the_excerpt() ?></div>
+                    </div>
+                </li>
+            <? endwhile ?>
+        </ul>
+    </body>
+</html>
